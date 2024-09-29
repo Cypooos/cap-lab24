77f89ac026d25aa1ad74108b5a3e99f1640be481
Cypooos <60020572+Cypooos@users.noreply.github.com>
2024-09-29 21:01:04 +0200

    Add nix env

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+let
+  riscvPkgs =
+    import <nixpkgs> {
+      # uses GCC and newlib
+      crossSystem = { system = "riscv64-none-elf"; };
+    };
+in
+
+riscvPkgs.mkShell {
+
+  packages = [
+    pkgs.hello
+    pkgs.antlr4_12
+    pkgs.python312
+    pkgs.pyright
+    pkgs.python312Packages.pytest
+    pkgs.python312Packages.pytest-xdist
+    pkgs.python312Packages.pytest-cov
+    pkgs.python312Packages.antlr4-python3-runtime 
+    pkgs.python312Packages.graphviz
+    pkgs.
+    pkgs.spike
+    pkgs.dtc
+    pkgs.pkgsCross.riscv64.riscv-pk
+  ];
+}
